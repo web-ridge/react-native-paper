c3fe212168ad6dce2a6746b0660b5bd84a27bd13
Robin Lindhout <robin.lindhout@webridge.nl>
2023-12-12 15:27:39 +0100

    fix: fix shadowOffset crash

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -232,7 +232,7 @@ const Chip = ({
     Animated.timing(elevation, {
       toValue: isV3 ? (elevated ? 2 : 0) : 4,
       duration: 200 * scale,
-      useNativeDriver: true,
+      useNativeDriver: false,
     }).start();
   });
 
@@ -243,7 +243,7 @@ const Chip = ({
     Animated.timing(elevation, {
       toValue: isV3 && elevated ? 1 : 0,
       duration: 150 * scale,
-      useNativeDriver: true,
+      useNativeDriver: false,
     }).start();
   });
 
