baea8894fca8ab7174e71a7cf5a290c595a088f2
Robin Lindhout <robin.lindhout@webridge.nl>
2025-08-07 14:13:29 +0200

    fix: try react compiler

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
 module.exports = {
   plugins: [
-    ['babel-plugin-react-compiler', { logLevel: 'verbose', target: 19 }],
+    [
+      'babel-plugin-react-compiler',
+      {
+        logLevel: 'verbose',
+
+        // compilationMode: 'all',
+        // panicThreshold: 'all_errors',
+        logger: {
+          logEvent(filename, event) {
+            if (event.kind === 'CompileSuccess') {
+              console.log('✔ Compiled:', filename);
+            } else {
+              console.warn('ℹ Compilation error in:', filename, event.reason);
+            }
+          },
+        },
+      },
+    ],
   ],
   presets: ['module:@react-native/babel-preset'],
 };
